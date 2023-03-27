47da03f04540e1963b9f9567877362dccfdc2c8e
Mamdasn <mhsaber1375@gmail.com>
2023-03-27 10:51:08 +0200

    Get input the nginx port number from user

--- telegram-bot-setup.sh
+++ telegram-bot-setup.sh
@@ -1,5 +1,16 @@
 read -p "Enter the IP address of your vps: " IP_ADDRESS
 read -p "Enter the http api token of your telegram bot: " TG_BOT_TOKEN
+read -p "Enter the port number for the telegram server to send the messages on: [80, 88, 443 or 8443]: " PORTSSL
+
+[ -z "$PORTSSL" ] && 
+
+if [ $port -eq 80 ] || [ $port -eq 88 ] || [ $port -eq 443 ] || [ $port -eq 8443 ]; then
+  echo "The port is valid."
+else
+  PORTSSL=443
+  echo "The port is not valid. Changing port to 443"
+fi
+
 
 echo Installing nginx to setup the portforwarding from ssl connections to flask
 sudo apt-get install -y nginx
@@ -10,7 +21,7 @@ cd SSL
 openssl req -newkey rsa:2048 -sha256 -nodes -keyout YOURPRIVATE.key -x509 -days 365 -out YOURPUBLIC.pem -subj "/C=US/ST=New York/L=Brooklyn/O=Example Brooklyn Company/CN=$IP_ADDRESS"
 SSL_PUBLIC=$(realpath YOURPUBLIC.pem)
 SSL_PRIVATE=$(realpath YOURPRIVATE.key)
-curl -F "ip_address=$IP_ADDRESS" -F "url=https://$IP_ADDRESS/" -F "certificate=@YOURPUBLIC.pem" https://api.telegram.org/bot$TG_BOT_TOKEN/setWebhook
+curl -F "ip_address=$IP_ADDRESS" -F "url=https://$IP_ADDRESS:$PORTSSL/" -F "certificate=@YOURPUBLIC.pem" https://api.telegram.org/bot$TG_BOT_TOKEN/setWebhook
 cd ..
 
 echo
@@ -20,8 +31,7 @@ sed -i "s/TG_BOT_TOKEN=\"Run telegram-bot-setup.sh\"/TG_BOT_TOKEN=\"$TG_BOT_TOKE
 
 cat << EOF | sudo tee /etc/nginx/sites-available/portforwarding.txt
 server {
-  listen 80;
-  listen 443 ssl;
+  listen $PORTSSL ssl;
   server_name $IP_ADDRESS;
   ssl_certificate $SSL_PUBLIC;
   ssl_certificate_key $SSL_PRIVATE;
