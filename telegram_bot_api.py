c6d1096a87ffe711ba9baa445e6c8ea24ce6301e
Mamdasan S <mhsaber1375@gmail.com>
2023-09-26 00:21:52 +0200

    Define Message Class for incoming messages

--- telegram_bot_api.py
+++ telegram_bot_api.py
@@ -4,6 +4,17 @@ from credentials import config
 token = config.TG_BOT_TOKEN
 base_link = f'https://api.telegram.org/bot{token}'
 
+class Message:
+    def __init__(self, message: dict):
+        self._message = message
+    def __getitem__(self, key):
+        keys = key if isinstance(key, tuple) else (key,)
+        message = self._message
+        for k in keys:
+            if not isinstance(message, dict): return None
+            message = message.get(k)
+        return message 
+
 def parse_message(msg):
     """
     Parses a message object received from the Telegram API.
@@ -14,21 +25,18 @@ def parse_message(msg):
     Returns:
         chat_id (int): The ID of the chat the message was sent in.
         message_info (list): A list containing information about the message.
-        message_type (str): Either 'callback_query' or 'message'.
+        message_type (str): 'message'.
     """
-    # message check
-    message = msg.get('message')
-    if message:
-        chat = message.get('chat')
-        chat_id = chat.get('id')
-        chat_type = chat.get('type')
-        if chat_type == 'private': 
-            message_id = message.get('message_id')
-            txt = message.get('text')
-            message_info = [txt, message_id]
-            return chat_id, message_info, 'message'
-
-    return None, None, None
+    msg = Message(msg)
+    message   = msg['message']
+    chat      = msg['message', 'chat']
+    chat_id   = msg['message', 'chat', 'id']
+    chat_type = msg['message', 'chat', 'type']
+    if chat_type == 'private': 
+        message_id = msg['message', 'message_id']
+        txt        = msg['message', 'text']
+        message_info = [txt, message_id]
+        return chat_id, message_info, 'message'
     
 def sendChatAction(chat_id, action='typing'): 
     """
