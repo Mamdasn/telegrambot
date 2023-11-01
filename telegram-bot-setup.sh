d96f127cc1c5387cefaf0f007ab6f7846db0f60a
Mamdasan S <mhsaber1375@gmail.com>
2023-11-01 04:48:39 +0100

    Replace the white listed ips inside nginx config
    file properly using sed

--- telegram-bot-setup.sh
+++ telegram-bot-setup.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
-[ -z $IP_ADDRESS ] &&
-	read -p "Enter the IP address of your vps: " IP_ADDRESS &&
-		sed -i "s/IP_ADDRESS\=/IP_ADDRESS\=$IP_ADDRESS/g" setenv.sh
-[ -z $TG_BOT_TOKEN ] &&
-	read -p "Enter the http api token of your telegram bot: " TG_BOT_TOKEN &&
-		sed -i "s/TG_BOT_TOKEN\=/TG_BOT_TOKEN\=$TG_BOT_TOKEN/g" setenv.sh
-
 PORTSSL=443
 
+[ -z $IP_ADDRESS ] &&
+	echo set the IP_ADDRESS by running: export IP_ADDRESS={IP_ADDRESS}. &&
+		return
+[ -z $TG_BOT_TOKEN ] &&
+	echo set the TG_BOT_TOKEN by running: export TG_BOT_TOKEN={TG_BOT_TOKEN}. &&
+		return
+
+
 echo Setting up ssl files
-mkdir SSLFILES
+mkdir -p SSLFILES
 cd SSLFILES
-openssl req -newkey rsa:8192 -sha256 -nodes -keyout YOURPRIVATE.key -x509 -days 365 -out YOURPUBLIC.pem -subj "/C=DE/ST=Berlin/L=Berlin/O=FERI Company/CN"
+openssl req -newkey rsa:8192 -sha256 -nodes -keyout YOURPRIVATE.key -x509 -days 365 -out YOURPUBLIC.pem -subj "/C=US/ST=New York/L=Brooklyn/O=Example Brooklyn Company/CN=8.8.8.8"
 SSL_PUBLIC=$(realpath YOURPUBLIC.pem)
 SSL_PRIVATE=$(realpath YOURPRIVATE.key)
 curl -F "ip_address=$IP_ADDRESS" -F "url=https://$IP_ADDRESS:$PORTSSL/" -F "certificate=@YOURPUBLIC.pem" https://api.telegram.org/bot$TG_BOT_TOKEN/setWebhook
 cd ..
 
+echo
 echo Downloading a list of telegram server ips to make a whitelist for nginx...
 IPLIST=$(curl -s https://core.telegram.org/resources/cidr.txt)
-WHITE_LISTED_IPS=$(echo "$IPLIST" | awk '{print "    allow " $0";"}')
-echo Done.
+WHITE_LISTED_IPS=$(echo "$IPLIST" | awk '{print "		allow " $0";"}')
+# Escape forward slashes and replace newlines with '\n' to prepare the data for sed
+WHITE_LISTED_IPS_ESCAPED=$(echo "$WHITE_LISTED_IPS" | sed 's#/#\\/#g' | awk '{printf "%s\\n", $0}')
 
 echo
 echo Setting up the white listed IPs in the nginx configuration file
-sed -i "s/WHITE_LISTED_IPS/$WHITE_LISTED_IPS/g" portforwarding.conf
+sed -i "s#WHITE_LISTED_IPS#$WHITE_LISTED_IPS_ESCAPED#g" portforwarding.conf
+echo Done.
 
 echo "Run this command to start the telegram bot: source setenv.sh && docker compose up"
