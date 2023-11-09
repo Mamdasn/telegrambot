b43434372ca04c8781e47dac95dc727bec5b2366
Mamdasan S <mhsaber1375@gmail.com>
2023-11-09 15:36:11 +0100

    Delete the redundant docker run file
    Since it is not necessary anymore.

--- run-docker-container.sh
+++
@@ -1,2 +0,0 @@
-docker build -t telegrambot:0.0.0 telegrambot
-docker run  -d -p 443:443 -e IP_ADDRESS=${IP_ADDRESS} -e TG_BOT_TOKEN=${TG_BOT_TOKEN} -v ./output:/app/output --name telegrambot telegrambot:0.0.0
