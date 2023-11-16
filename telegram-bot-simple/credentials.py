5e0101de0233ed73ce32886571f10304fc324403
Mamdasan S <mhsaber1375@gmail.com>
2023-11-16 15:43:33 +0100

    Update the documentation for credentials py

--- telegram-bot-simple/credentials.py
+++ telegram-bot-simple/credentials.py
@@ -1,5 +1,16 @@
 import os
 
-
 class config:
+    """
+    A configuration class for storing environment-specific settings.
+
+    This class is designed to hold configuration variables that are essential
+    for the application, such as tokens and keys. These variables are typically
+    loaded from the environment to ensure security and flexibility across different
+    deployment environments.
+
+    Class Attributes:
+        TG_BOT_TOKEN (str): Telegram Bot Token, fetched from the environment variable 'TG_BOT_TOKEN'.
+                            This token is used to authenticate and interact with the Telegram Bot API.
+    """
     TG_BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
