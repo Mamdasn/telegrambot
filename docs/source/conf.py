a30cfa54104ab879fcb535cb4d22e1966bc2a018
Mamdasan S <mhsaber1375@gmail.com>
2023-12-23 14:32:14 +0100

    Sort the methods in libs based on the source

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,9 @@ master_doc = "index"
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
 
+# Set sorting of methods based on the source (default is alphabetical)
+autodoc_member_order = "bysource"
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
